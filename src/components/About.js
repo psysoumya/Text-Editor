cdcfe15249d99b9b5df434d3b180924b82e8d6a0
Soumya Ranjan Nayak <108055846+psysoumya@users.noreply.github.com>
2022-08-03 15:33:22 +0530

    Text-Editor

---
+++ src/components/About.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react'
+
+export default function About() {
+
+    const [myStyle, setMyStyle] = useState({
+        color: 'black',
+        backgroundColor: 'white'
+    })
+    const [btntext, setBtnText] = useState("Enable Dark Mode")
+    
+    const toggleStyle = ()=>{
+        if(myStyle.color === 'black'){
+            setMyStyle({ 
+                color: 'white',
+                backgroundColor: 'black',
+                border: '1px solid white'
+                
+            })
+            setBtnText("Enable Light Mode")
+        }
+        else{
+            setMyStyle({ 
+                color: 'black',
+                backgroundColor: 'white'
+            })
+            setBtnText("Enable Dark Mode");
+        }
+    }
+    
+    return (
+        <div className="container" style={myStyle}>
+            <h3 className="my-3">About Us</h3>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        This webpage Designed using React
+                    </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                        <strong>Here you can Convert your text from Lower to Upper and vice versa,word count, character count, remove extra spaces etc.</strong> 
+                    </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="container my-3">
+                <button onClick={toggleStyle} type="button" className="btn btn-primary">{btntext}</button>
+            </div>
+        </div>
+    )
+}
